36e35d3b1f953aa891926953053de1c221cd69bd
Madhur Chavan <chavanmadhur65@gmail.com>
2022-08-11 15:57:34 +0530

    xsffwdw

---
+++ WebAPI/WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAPI.Models;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Class1 data)
+        {
+            int PageID = data.PageID;
+            int ProjectID = data.ProjectID;
+            string HelpTitle = data.HelpTitle;
+            string HelpBody = data.HelpBody;
+            int flag = 1;
+            DataTable dt = new DataTable();
+            string query = @"INSERT INTO dbo.HELP VALUES(
+            " + PageID+ @",
+            " + ProjectID + @",
+            '" + HelpTitle + @"',
+            '" + HelpBody + @"',
+             " + flag + @"
+
+            )";
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["HELPDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(dt);
+            }
+
+            return Redirect("/admin/index");
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Class1
+    {
+        ///<summary>
+        /// Gets or sets PersonId.
+        ///</summary>
+        public int ID { get; set; }
+
+        public int PageID { get; set; }
+
+        ///<summary>
+        /// Gets or sets Name.
+        ///</summary>
+        public int ProjectID { get; set; }
+
+
+        ///<summary>
+        /// Gets or sets Gender.
+        ///</summary>
+        public string HelpTitle { get; set; }
+
+        ///<summary>
+        /// Gets or sets City.
+        ///</summary>
+        public string HelpBody { get; set; }
+    }
+}
