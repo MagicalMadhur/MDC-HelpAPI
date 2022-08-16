e3c64da6d6c104ffbf4868005b4b468a8bed7ec7
Madhur Chavan <chavanmadhur65@gmail.com>
2022-08-16 12:54:27 +0530

    new

---
+++ WebAPI/WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAPI.Models;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Class1 data)
+        {
+            int PageID = data.PageID;
+            int ProjectID = data.ProjectID;
+            string HelpTitle = data.HelpTitle;
+            string HelpBody = data.HelpBody;
+            int flag = 1;
+            DataTable dt = new DataTable();
+            string query = @"execute InsertData @PageID="+PageID+",@ProjectID="+ProjectID+",@HelpTitle='"+HelpTitle+"',@HelpBody='"+HelpBody+"',@flag="+flag+"";
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["HELPDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(dt);
+            }
+
+            return Redirect("/admin/index");
+        }
+    }
+}
